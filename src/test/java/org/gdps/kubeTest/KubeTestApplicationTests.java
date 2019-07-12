43d0c2fdc6e80760ef80e51e6a7482e68fd18f6a
Gabriel David <gabrieldps@gmail.com>
2019-07-12 15:54:54 -0300

    First commit

---
+++ src/test/java/org/gdps/kubeTest/KubeTestApplicationTests.java
@@ -0,0 +1,16 @@
+package org.gdps.kubeTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KubeTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
